c23a2dc17a633450dead46ef3cde36a3b53b7fca
Rahul Dixit <125376372+hackpure@users.noreply.github.com>
2023-02-21 20:56:06 +0530

    Largest Among Numbers

---
+++ largestamongnumbers.c
@@ -0,0 +1,11 @@
+// Largest Among Numbers
+
+#include <stdio.h>
+
+int main(){
+    int a, b, c, max;
+    scanf("%d%d%d", &a, &b, &c);
+    max = (a > b) ? (a > c ? a : c) : (b > c ? b : c);
+    printf("%d", max);
+    return 0;
+}
